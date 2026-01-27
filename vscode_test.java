56ada73ba5ad68301f92c0df6a4b9aa8bffd0d8f
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-01-27 19:43:58 +0900

    Add files via upload

--- vscode_test.java
+++ vscode_test.java
@@ -2,21 +2,53 @@ import java.util.Scanner;
 
 public class vscode_test {
     public static void main(String[] args) {
+        /*
+         * 회문검사기
+         * 
+         * 입력: level
+         * 출력: 회문입니다!
+         * 
+         * 입력: hello
+         * 출력: 회문이 아닙니다.
+         * 
+         * 입력: noon
+         * 출력: 회문입니다!
+         * 
+         * 입력: racecar
+         * 출력: 회문입니다!
+         * 
+         * 회문 변수 기준을 1로.
+         * naen -> 기준을 0으로 두면 앞-끝 자리가 같아도 회문으로 출력된다.
+         */
+
+        // String[] arr = new String[50];
+
+        System.out.println("단어 입력 : ");
         Scanner sc = new Scanner(System.in);
+        // 단어 입력받을 string변수
+        String str = sc.next().toLowerCase();
 
-        String[] arr = new String[5];
+        // 입력받은 문자 길이
+        int length = str.length();
 
-        System.out.println("문자입력:");
+        // 회문 인지 판별
+        // 회문 변수
+        int palindrome = 1;
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.next();
+        for (int i = 0; i < length / 2; i++) {
+            if (str.charAt(i) != str.charAt(length - i - 1)) {
+                palindrome = 0;
+                break;
+            }
         }
-        sc.close();
 
-        System.out.println("=========");
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        if (palindrome == 1) {
+            System.out.println("회문입니다.");
+        } else {
+            System.out.println("회문이 아닙니다.");
         }
+        // System.out.println("개수는 " + length);
+        // System.out.println("글자 : " + str);
+
     }
 }

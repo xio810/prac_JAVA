68eebc0fd23625255f7472feae5ba03b5892ff0e
0ㅐ옹 <69510098+xio810@users.noreply.github.com>
2026-02-21 22:32:23 +0900

    scanner, for문, 배열

--- vscode_test.java
+++ vscode_test.java
@@ -2,53 +2,39 @@ import java.util.Scanner;
 
 public class vscode_test {
     public static void main(String[] args) {
-        /*
-         * 회문검사기
-         * 
-         * 입력: level
-         * 출력: 회문입니다!
-         * 
-         * 입력: hello
-         * 출력: 회문이 아닙니다.
-         * 
-         * 입력: noon
-         * 출력: 회문입니다!
-         * 
-         * 입력: racecar
-         * 출력: 회문입니다!
-         * 
-         * 회문 변수 기준을 1로.
-         * naen -> 기준을 0으로 두면 앞-끝 자리가 같아도 회문으로 출력된다.
-         */
 
         // String[] arr = new String[50];
-
-        System.out.println("단어 입력 : ");
-        Scanner sc = new Scanner(System.in);
+        // Scanner sc = new Scanner(System.in);
         // 단어 입력받을 string변수
-        String str = sc.next().toLowerCase();
+        // String str = sc.next().toLowerCase();
 
-        // 입력받은 문자 길이
-        int length = str.length();
+        System.out.println("배열 길이 입력 : ");
 
-        // 회문 인지 판별
-        // 회문 변수
-        int palindrome = 1;
+        Scanner sc = new Scanner(System.in); // 입력받는 도구 생성
+        int length = sc.nextInt(); // scanf 배열길이 입력
 
-        for (int i = 0; i < length / 2; i++) {
-            if (str.charAt(i) != str.charAt(length - i - 1)) {
-                palindrome = 0;
-                break;
-            }
+        int[] arr = new int[length]; // length길이에 맞는 정수형 배열 생성
+
+        // 배열 안에 숫자 입력
+        for (int i = 0; i < length; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        if (palindrome == 1) {
-            System.out.println("회문입니다.");
-        } else {
-            System.out.println("회문이 아닙니다.");
+        // 출력
+        for (int i = 0; i < length; i++) {
+            System.out.print(arr[i] + " ");
         }
-        // System.out.println("개수는 " + length);
-        // System.out.println("글자 : " + str);
 
+        /*
+         * int arr[10] = {0};
+         * 
+         * for(int i = 0; i < length; i++){
+         * scanf("%d", arr[i]);
+         * }
+         * 
+         * for(int i = 0; i < length; i++){
+         * printf("%d ", arr[i]);
+         * }
+         */
     }
 }
